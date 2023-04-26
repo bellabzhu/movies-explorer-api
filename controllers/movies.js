271d8463169f59899df0fbf879ce9d63c2c665ca
bellabzhu <bellabzhu@gmail.com>
2023-04-26 17:21:43 +0000

    del console.log

--- controllers/movies.js
+++ controllers/movies.js
@@ -32,7 +32,6 @@ module.exports.createMovie = async (req, res, next) => {
 module.exports.delMovie = async (req, res, next) => {
   try {
     const movie = await Movie.findById(req.params.id);
-    console.log(req.params.id);
     if (!movie) {
       next(new Error404('Фильм с таким таким id не найден.'));
       return;
